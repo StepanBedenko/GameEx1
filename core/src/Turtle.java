84dad55381d740ab4c9d06a17dc640c2a0bd717d
Stepan Bedenko <stepanbedenko@gmail.com>
2021-04-08 00:21:36 +0300

    sets are true now

--- core/src/Turtle.java
+++ core/src/Turtle.java
@@ -7,8 +7,8 @@ public class Turtle extends BaseActor{
     public Turtle(float x, float y, Stage s){
         super(x,y,s);
         setAcceleration(200);
-        setMaxSpeed(10);
-        setDeceleration(400);
+        setMaxSpeed(400);
+        setDeceleration(200);
         String[] fileNames =
                 {"turtle-1.png", "turtle-2.png", "turtle-3.png",
                         "turtle-4.png", "turtle-5.png", "turtle-6.png"};
